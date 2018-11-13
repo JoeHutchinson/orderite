89a3bd0ac551072e88ec67cafe1deedef619d632
Joe <13831451+JoeHutchinson@users.noreply.github.com>
2018-11-13 16:13:34 +0000

    Add Basket + Item entities.

---
+++ Core/Basket/Basket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Basket
+{
+    public class Basket : Entity
+    {
+        public string BuyerId { get; set; }
+        private readonly List<Item> _items = new List<Item>();
+        public IReadOnlyCollection<Item> Items => _items.AsReadOnly();
+
+        public void AddItem(int catalogueItemId, decimal unitPrice, int quantity = 1)
+        {
+            if (Items.All(i => i.CatalogueItemId != catalogueItemId))
+            {
+                _items.Add(
+                    new Item
+                    {
+                        CatalogueItemId = catalogueItemId,
+                        Quantity = quantity,
+                        UnitPrice = unitPrice
+                    }
+                );
+                return;
+            }
+
+            var existingItem = Items.FirstOrDefault(i => i.CatalogueItemId == catalogueItemId);
+            existingItem.Quantity += quantity;
+        }
+    }
+}
---
+++ Core/Basket/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Basket
+{
+    public class Item : Entity
+    {
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int CatalogueItemId { get; set; }
+    }
+}
