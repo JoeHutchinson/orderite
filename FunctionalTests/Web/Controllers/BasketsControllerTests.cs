4467f10f489a2c65ef0feae05d1c615bcc2666c0
Joe <13831451+JoeHutchinson@users.noreply.github.com>
2018-11-15 17:35:21 +0000

    Functional test unknown basket

--- FunctionalTests/Web/Controllers/BasketsControllerTests.cs
+++ FunctionalTests/Web/Controllers/BasketsControllerTests.cs
@@ -1,9 +1,11 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Web;
 using Web.ApiModels;
 
@@ -13,7 +15,7 @@ namespace FunctionalTests.Web.Controllers
     public class BasketsControllerTests
     {
         [TestMethod]
-        public async Task CanCreateBasket()
+        public async Task CreateBasket()
         {
             var factory = new WebApplicationFactory<Startup>();
             var client = factory.CreateClient();
@@ -21,6 +23,15 @@ namespace FunctionalTests.Web.Controllers
             content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             var response = await client.PostAsync("/api/joe/baskets", content);
             response.EnsureSuccessStatusCode();
+        }
+
+        [TestMethod]
+        public async Task GetBasketUnknownBasket()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+            var response = await client.GetAsync("/api/joe/baskets/1");
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
         }
     }
 }
